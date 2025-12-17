88d7ae2b6892ae6ca33541c1ee97cad13f413f06
unknown <mr.shahbaznns@gmail.com>
2025-12-17 13:16:13 +0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Tenant\Auth\LoginController;
+use App\Http\Controllers\Tenant\Auth\RegisterController;
 use App\Http\Controllers\Tenant\DashboardController;
 use Illuminate\Support\Facades\Route;
 
